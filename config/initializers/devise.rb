71c19009d062139850db4ab35756fbed897311c7
Adam Winn <adam.winn@eimprovement.com>
2017-07-08 19:48:26 -0500

    mailgun

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'nniw@mada@gmail.com'
+  config.mailer_sender = 'postmaster@sandbox7d6743916360441e9c7a401ad39fd8e2.mailgun.org'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
