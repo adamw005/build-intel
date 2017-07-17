0c891ad5ca7ac621c3207c139800d35c1a5b1504
Adam Winn <adam.winn@eimprovement.com>
2017-07-17 14:36:21 -0500

    update dashboard

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,6 +1,3 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
 $(document).ready(function () {
     $(".dashboard a").mouseover(function () {
         $(".dashboard a img").css("display", "none"); // hide all product images
