a1e9a7a4ee604b9bf92a2a0849aed8660642bfa1
Adam Winn <adam.winn@eimprovement.com>
2017-07-17 15:09:51 -0500

    update dashboard

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     $(".dashboard a").mouseover(function () {
-        $(".dashboard a img").css("display", "none"); // hide all product images
-        $(this).find("img").css("display", "inline-block"); // show current hover image
+        $(".dashboard a .popup_chart").css("display", "none"); // hide all product images
+        $(this).find(".popup_chart").css("display", "inline-block"); // show current hover image
     })
     $(".dashboard a").mouseout(function () {
-        $(".dashboard a img").css("display", "none"); // hide all product images
+        $(".dashboard a .popup_chart").css("display", "none"); // hide all product images
     })
 });
