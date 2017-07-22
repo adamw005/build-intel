1fc06fa0823651c0fad5b97c1a04a3da256b4795
Adam Winn <adam.winn@eimprovement.com>
2017-07-22 16:12:27 -0500

    fixing datatables

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,8 +1,10 @@
 class PricesController < ApplicationController
 
-  respond_to do |format|
-    format.html
-    format.json { render json: PriceDatatable.new(view_context) }
+  def index
+    respond_to do |format|
+      format.html
+      format.json { render json: PriceDatatable.new(view_context) }
+    end
   end
 
 end
