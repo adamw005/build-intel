5af94e073b36b37c031d50219055c4dc5a8d25a6
Adam Winn <adam.winn@eimprovement.com>
2017-07-22 17:20:25 -0500

    bootstrap table

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require dataTables/jquery.dataTables
-//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //  require turbolinks
 //= require Chart.bundle
 //= require chartkick
