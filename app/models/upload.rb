f86c015e7ba0d4db9a9eeaad62bceccc36b01fd8
Adam Winn <adam.winn@eimprovement.com>
2017-07-23 13:33:28 -0500

    upload

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,4 +1,4 @@
 class Upload < ActiveRecord::Base
-  has_attached_file :csv_file, url: ':s3_path_url', path: "/:class/avatars/:id_:basename.:style.:extension"
+  has_attached_file :csv_file
   do_not_validate_attachment_file_type :csv_file
 end
